356c5f14a46c42a6408b4b3300ba00474e6fe965
CheeryPhantom <sujaltrin@gmail.com>
2026-03-14 22:36:01 +0545

    fixed environment variable

--- api/index.js
+++ api/index.js
@@ -1,2 +1,6 @@
-import app from '../backend/src/index.js'
-export default app;
+// File: [api/index.js](api/index.js)
+import app from '../backend/src/index.js';
+
+export default function handler(req, res) {
+  return app(req, res);
+}
